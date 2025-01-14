debbbf6f34db68d5ecf02404126b714b6c67bc18
TOomaAh <tbrunet72@gmail.com>
2025-01-15 00:48:59 +0100

    add theme

--- cmd/mediatools/main.go
+++ cmd/mediatools/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	a := app.New()
+
+	a.Settings().SetTheme(newMediaToolsTheme())
+
+	w := a.NewWindow("Media Tools")
+	w.Resize(fyne.NewSize(800, 600))
+
+	w.SetContent(container.NewVBox(
+		widget.NewButton("Button", func() {}),
+		widget.NewButton("Disabled", func() {}),
+	))
+
+	w.ShowAndRun()
 }
---
+++ cmd/mediatools/theme.go
@@ -0,0 +1,51 @@
+//go:generate fyne bundle -o bundled.go ../../assets
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+type mediaToolsTheme struct {
+	fyne.Theme
+}
+
+func newMediaToolsTheme() fyne.Theme {
+	return &mediaToolsTheme{Theme: theme.DefaultTheme()}
+}
+
+func (m *mediaToolsTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		return color.RGBA{R: 0x22, G: 0x22, B: 0x22, A: 0xff}
+	}
+	return m.Theme.Color(name, variant)
+}
+
+func (t *mediaToolsTheme) Font(s fyne.TextStyle) fyne.Resource {
+	if s.Symbol || s.Monospace {
+		return t.Theme.Font(s)
+	}
+
+	if s.Bold {
+		if s.Italic {
+			return resourcePoppinsBoldItalicTtf
+		} else {
+			return resourcePoppinsBoldTtf
+		}
+	}
+	if s.Italic {
+		return resourcePoppinsItalicTtf
+	}
+	return resourcePoppinsRegularTtf
+}
+
+func (t *mediaToolsTheme) Size(name fyne.ThemeSizeName) float32 {
+	// Ajustement des tailles pour un design compact
+	if name == theme.SizeNameText {
+		return 12
+	}
+
+	return t.Theme.Size(name)
+}
